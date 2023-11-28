c5eb201af4b4cac0e3f41d376206383283b1588a
Hussein Aladdin <haladdin13@outlook.com>
2023-11-28 15:11:24 -0500

    adds for and while loop

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  const card = [];
+  for (let i = 0; i < names.length; i++) {
+    card.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    debugger;
+  }
+  return card;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
